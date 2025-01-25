7fa83aaf853f008edc65e1e24e5304770481456d
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2025-01-25 14:37:24 -0600

    .

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_and_preprocess_data():
 
 
 def main():
-    st.title("Find Your Ammo! 🎯")
+    st.title("Find Ammos 🎯")
     st.markdown("### Your one-stop shop for the best ammo prices")
 
     df = load_and_preprocess_data()
